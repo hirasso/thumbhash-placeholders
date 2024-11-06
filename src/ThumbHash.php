763c0116d3405d7170700fbb7c6e3c01dff5a44b
Rasso Hilber <mail@rassohilber.com>
2024-11-06 21:26:03 +0100

    Fix case for ThumbHash

---
+++ src/ThumbHash.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Hirasso\WP\ThumbhashPlaceholders;
+
+use Exception;
+use Hirasso\WP\ThumbhashPlaceholders\Enums\ImageDriver;
+use RuntimeException;
+use WP_Image_Editor;
+use WP_Error;
+use Thumbhash\Thumbhash as ThumbhashLib;
+
+use function Thumbhash\extract_size_and_pixels_with_gd;
+use function Thumbhash\extract_size_and_pixels_with_imagick;
+
+class ThumbHash
+{
+    /**
+     * Generate a thumbhash from a file
+     */
+    public static function encode(
+        string $file,
+        string $mimeType
+    ): string {
+        if (!file_exists($file)) {
+            throw new RuntimeException(sprintf(
+                /* translators: %s is a path to a file */
+                __('File not found: %s', 'thumbhash-placeholders'),
+                esc_html($file)
+            ));
+        }
+
+        /** @var WP_Image_Editor|WP_Error */
+        $editor = wp_get_image_editor($file, [
+            'mime_type' => $mimeType,
+        ]);
+
+        if (is_wp_error($editor)) {
+            return $editor;
+        }
+
+        [$width, $height, $pixels] = static::extractSizeAndPixels(
+            driver: static::getImageDriver($editor),
+            image: static::getDownsizedImage($editor, get_post_mime_type($file, $mimeType))
+        );
+
+        $hash = ThumbhashLib::RGBAToHash($width, $height, $pixels);
+        return ThumbhashLib::convertHashToString($hash);
+    }
+
+    /**
+     * Decode a stored hash
+     */
+    public static function getDataURI(
+        string $hashString
+    ): string|null {
+        if (empty($hashString)) {
+            return null;
+        }
+
+        try {
+
+            $hashArray = ThumbhashLib::convertStringToHash($hashString);
+            return ThumbhashLib::toDataURL($hashArray);
+        } catch (Exception $e) {
+
+            throw new RuntimeException(sprintf(
+                /* translators: %s is an exception message */
+                __('Error decoding thumbhash: %s', 'thumbhash-placeholders'),
+                $e->getMessage()
+            ));
+        }
+    }
+
+    /**
+     * Get a resized version of an image
+     */
+    private static function getDownsizedImage(
+        WP_Image_Editor $editor,
+        string $mimeType
+    ): string {
+        $editor->resize(32, 32, false);
+
+        // Save the image to a temporary location
+        $tempFile = wp_tempnam();
+
+        if (is_wp_error($saved = $editor->save($tempFile, $mimeType))) {
+            throw new \RuntimeException('Failed to save resized image.');
+        }
+
+        $file = $saved['path'];
+
+        // Check if the file exists and is readable
+        if (!file_exists($file) || !is_readable($file)) {
+            throw new \RuntimeException('Temporary image file is not accessible.');
+        }
+
+        // Get the raw image data
+        $imageData = file_get_contents($file);
+
+        // Clean up the temporary file
+        wp_delete_file($tempFile);
+
+        return $imageData;
+    }
+
+    /**
+     * Extract the size and pixels from an image
+     */
+    private static function extractSizeAndPixels(
+        ImageDriver $driver,
+        string $image
+    ): array {
+        return match ($driver) {
+            ImageDriver::IMAGICK => extract_size_and_pixels_with_imagick($image),
+            ImageDriver::GD => extract_size_and_pixels_with_gd($image),
+            default => throw new Exception("Couldn't generate thumbhash data")
+        };
+    }
+
+    /**
+     * Get the current image driver
+     */
+    private static function getImageDriver(
+        WP_Image_Editor $editor
+    ): ImageDriver {
+        return match ($editor::class) {
+            'WP_Image_Editor_Imagick' => ImageDriver::IMAGICK,
+            'WP_Image_Editor_GD' => ImageDriver::GD,
+            default => throw new Exception("Unsupported image driver")
+        };
+    }
+}
