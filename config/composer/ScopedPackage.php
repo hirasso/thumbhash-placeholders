38fa09f57b0c6af49e1eb3338faeeac9d703be3b
Rasso Hilber <mail@rassohilber.com>
2024-11-07 10:21:29 +0100

    Make the composer actions more beautifuller 😆

--- config/composer/ScopedPackage.php
+++ config/composer/ScopedPackage.php
@@ -5,31 +5,62 @@ namespace Hirasso\WP\ComposerActions;
 use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
 use Composer\Script\Event;
+use Exception;
 
-class ScopedPackage
+class ScopedPackage extends ComposerAction
 {
     /**
-     * Used to post process the scoped release package.
+     * Post process the scoped release package.
      * Currently, the project's composer.json is being copied
      * to the scoped folder and all dependencies are being removed
      * from it. This should help with better collision prevention 🤞
      */
     public static function postProcess(Event $event)
     {
-        $composer = $event->getComposer();
         $io = $event->getIO();
 
-        $io->write("Postprocessing scoped package...");
+        if (!is_dir('build')) {
+            $io->error("The build folder doesn't exist");
+        }
 
-        $composerJSON = static::generateComposerJSON($io);
-        $scopedJSONFile = new JsonFile('scoped/composer.json');
-        $scopedJSONFile->write($composerJSON);
+        $io->write("<info>Postprocessing scoped package...</info>");
 
-        $io->write("Postprocessing complete!");
+        $rootDir = static::getRootDir($event);
+
+        /**
+         * Cleanup stuff not required in the release
+         */
+        static::remove(
+            "$rootDir/build/composer.json",
+            "$rootDir/build/composer.lock",
+            "$rootDir/build/vendor/sniccowp/php-scoper-wordpress-excludes"
+        );
+
+        /**
+         * Rename scoped to whatever the root dir is called.
+         * `my-plugin/scoped` will become `my-plugin/my-plugin`
+         */
+        $newName = basename($rootDir);
+        static::renameFolder("$rootDir/build", $newName);
+
+        $io->write("<info>Renamed scoped folder from 'build' top '$newName'!</info>");
+
+        /**
+         * Generate a scoped composer.json, without dependencies
+         * This could come in handy for a scoped release to packagist
+         */
+        $composerJSON = static::generateScopedComposerJSON($io);
+        $jsonFile = new JsonFile("$rootDir/$newName/composer.json");
+        $jsonFile->write($composerJSON);
+
+        $io->write("<info>Created scoped composer.json!</info>");
     }
 
-    private static function generateComposerJSON(IOInterface $io): array
+    private static function generateScopedComposerJSON(IOInterface $io): array
     {
+        if (!file_exists('composer.json')) {
+            throw new Exception('composer.json not found at root directory');
+        }
 
         // Load the composer.json
         $jsonFile = new JsonFile('composer.json');
@@ -42,6 +73,8 @@ class ScopedPackage
             'config',
             'autoload-dev',
             'autoload',
+            'post-install-cmd',
+            'scripts-descriptions',
         ];
 
         foreach ($removeProperties as $key) {
